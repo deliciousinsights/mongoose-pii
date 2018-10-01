effcb9ede4dc0eb99a8b1e60f4eaeb082b0acbc9
Christophe Porteneuve <tdd@tddsworld.com>
2018-10-01 10:59:21 +0200

    feat(utils): expose helpers straight from the top-level module

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const { checkPassword, hashPassword } = require('./util/passwords')
+const { cipher, decipher } = require('./util/ciphers')
 const { markFieldsAsPII } = require('./markFieldsAsPII')
 
 module.exports = {
+  checkPassword,
+  cipher,
+  decipher,
+  hashPassword,
   markFieldsAsPII,
 }
